049f4222039ee274d3530c89c4683ec74d7afd92
pavel <pavel.pronchatov155@gmail.com>
2020-03-06 23:07:34 +0400

    feat: add morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arrCode = [];
+    let result = [];
+    let arrStr;
 
+
+    for(let i = 0; i < expr.length; i+=10){
+        arrCode.push(expr.slice(i,i + 10));
+    }
+
+
+
+    for(let prop in MORSE_TABLE){
+        arrStr = prop.split("");
+        for(let i = 0; i < arrStr.length; i++){
+            if(arrStr[i] == "."){
+                arrStr[i] = 10;
+            }else if (arrStr[i] == '-'){
+                arrStr[i] = 11;
+            }
+
+            if(arrStr.length < 5 ){
+                arrStr.unshift("00");
+            }
+        }
+        arrStr = arrStr.join("");
+        MORSE_TABLE[arrStr] = MORSE_TABLE[prop];
+    }
+    for(let i = 0; i < arrCode.length; i++){
+        for (let prop in MORSE_TABLE) {
+            
+            if(arrCode[i] == prop){
+                result.push(MORSE_TABLE[prop]);
+               
+            }
+        }
+        if(arrCode[i] == "**********"){
+            result.push(" ");
+        }
+    }
+    return result.join("");
+}
 module.exports = {
     decode
-}
+};
+
+
+
